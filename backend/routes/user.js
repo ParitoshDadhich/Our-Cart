ce12702a6b944c113d2640853aad81d299082b84
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-08-18 03:47:43 +0530

    initial commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-// when I was doing an assignment
-//const { getUserById, getUser, getAllUsers } = require("../controllers/user");
+ 
 const { getUserById, getUser, updateUser, userPurchaseList} = require("../controllers/user");
 const {isAdmin, isAuthenticated, isSignedIn} = require("../controllers/auth");
 
@@ -16,7 +15,6 @@ router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);  // getting i
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);   // here updating the user
 router.get("/orders/user/:userId", isAuthenticated, isSignedIn, userPurchaseList);  // listing the user purchase list
 
-// assignment
-// router.get("/users", getAllUsers);
+ 
 
 module.exports = router;
